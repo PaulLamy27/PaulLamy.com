95e1ba3e6f2b7611f36cc0a7b9cf06662faf0be4
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2024-08-29 13:39:15 -0400

    Navbar draft

---
+++ PaulLamy.com/src/navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="navbar">
+                <strong id="paul-lamy">ik ben het Navbar</strong>
+                <div className='link-icons'>
+                    <h5>LinkedIn</h5>
+                    <h5>gitHub</h5>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
