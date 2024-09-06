12adba3f459b5f7c9ceeffe7aaf2c55a15e68048
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2024-09-05 22:08:08 -0400

    navbar hyperlinks

--- PaulLamy.com/src/navbar/Navbar.tsx
+++ PaulLamy.com/src/navbar/Navbar.tsx
@@ -1,15 +1,35 @@
 import './Navbar.css'
 
+const redirect = (location: string) => {
+    switch (location) {
+        case "LinkedIn":
+            window.location.href = 'https://www.linkedin.com/in/paullamy/';
+            break;
+        case "GitHub":
+            window.location.href = 'https://github.com/PaulLamy27/';
+            break;
+        case "Resume":
+            window.location.href = 'https://www.linkedin.com/in/paullamy/';
+            break;
+        case "Dev.to":
+            window.location.href = 'https://www.linkedin.com/in/paullamy/';
+            break;
+        default:
+            console.error("something happened with the switch!");
+            break;
+    }
+}
+
 const Navbar = () => {
     return (
         <>
             <div className="navbar">
                 <strong id="paul-lamy">PaulLamy.com</strong>
                 <div className='link-icons'>
-                    <h5>LinkedIn</h5>
-                    <h5>GitHub</h5>
-                    <h5>Resume</h5>
-                    <h5>Dev.to</h5>
+                    <h5 onClick={() => redirect("LinkedIn")}>LinkedIn</h5>
+                    <h5 onClick={() => redirect("GitHub")}>GitHub</h5>
+                    <h5 onClick={() => redirect("Resume")}>Resume</h5>
+                    <h5 onClick={() => redirect("Dev.to")}>Dev.to</h5>
                 </div>
             </div>
         </>
