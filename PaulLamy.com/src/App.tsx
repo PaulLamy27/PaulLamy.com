0e3a6527422c2b623d7f2c82de9d7fe41123ba43
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2024-09-09 16:25:16 -0400

    CHORE: pushing modified files after successful deploy

--- PaulLamy.com/src/App.tsx
+++ PaulLamy.com/src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import './index.css'
+import '../index.css'
 import Navbar from './navbar/Navbar'
 import Hero from './hero/Hero'
 import Intro from './intro/Intro'
--- PaulLamy.com/vite.config.ts
+++ PaulLamy.com/vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // Ensures build output goes to 'dist'
+  build: {	
+	rollupOptions: {
+		input: './index.html',
+	},
+	outDir: 'dist' // Ensures build output goes to 'dist'
   }
 })
